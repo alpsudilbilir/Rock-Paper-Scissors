d34ea96bd6e9b9ba3a0b1172d3a7993e2edabf6b
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2021-10-14 23:42:33 +0300

    Initial commit.

---
+++ Rock-Paper-Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Alpsu Dilbilir on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
